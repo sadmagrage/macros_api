b37a2ee2f31e1adc766d50c32d6fb4f8fc02234c
sadmagrage <victorgoyapvgp09@gmail.com>
2025-06-06 20:06:50 -0300

    chore: create initial spring project files

---
+++ src/main/java/com/sadmag/macros_v2/MacrosV2Application.java
@@ -0,0 +1,18 @@
+package com.sadmag.macros_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		SecurityAutoConfiguration.class
+})
+public class MacrosV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrosV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadmag/macros_v2/MacrosV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadmag.macros_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrosV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
