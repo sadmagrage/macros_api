f61677a6119bc190a91b29e698265e59434b500d
sadmagrage <victorgoyapvgp09@gmail.com>
2025-06-10 00:50:03 -0300

    feature: create food entity

---
+++ src/main/java/com/sadmag/macros_v2/food/Food.java
@@ -0,0 +1,35 @@
+package com.sadmag.macros_v2.food;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "food_entity")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private float carb;
+
+    @Column
+    private float prot;
+
+    @Column
+    private float fat;
+
+    @Column
+    private String image;
+}
