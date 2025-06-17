b6189b21791b2270c0498f34e038d10f47bd31e8
sadmagrage <victorgoyapvgp09@gmail.com>
2025-06-17 01:01:43 -0300

    feature: add user info entity

---
+++ src/main/java/com/sadmag/macros_v2/user_info/UserInfo.java
@@ -0,0 +1,53 @@
+package com.sadmag.macros_v2.user_info;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sadmag.macros_v2.equation.EquationPreference;
+import com.sadmag.macros_v2.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_info_table")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private float weight;
+
+    @Column
+    private float bodyfat;
+
+    @Column
+    private LocalDateTime birth;
+
+    @Column
+    private int height;
+
+    @Column
+    private char gender;
+
+    @Column
+    private float activityFactor;
+
+    @Column
+    private EquationPreference equationPreference;
+
+    @Column(nullable = false)
+    private boolean macroInfoPublic;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JsonIgnoreProperties("user_info")
+    private User user;
+}
