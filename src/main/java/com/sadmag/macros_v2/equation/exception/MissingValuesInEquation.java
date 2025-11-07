713d51280c979e591eb9658e0ef81d80cee1e744
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-07 10:36:50 -0300

    feat: add exception to throw when params are missing calculate method

---
+++ src/main/java/com/sadmag/macros_v2/equation/exception/MissingValuesInEquation.java
@@ -0,0 +1,7 @@
+package com.sadmag.macros_v2.equation.exception;
+
+public class MissingValuesInEquation extends RuntimeException {
+    public MissingValuesInEquation(String message) {
+        super(message);
+    }
+}
