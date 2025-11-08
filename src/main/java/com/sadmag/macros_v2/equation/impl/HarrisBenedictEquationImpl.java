7e8b2abc3b1a45614f8217f5e32b2301a08f28e2
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-08 03:45:16 -0300

    fix: use datetime from dateUtils to become easier to mock in tests

--- src/main/java/com/sadmag/macros_v2/equation/impl/HarrisBenedictEquationImpl.java
+++ src/main/java/com/sadmag/macros_v2/equation/impl/HarrisBenedictEquationImpl.java
@@ -1,8 +1,10 @@
 package com.sadmag.macros_v2.equation.impl;
 
+import com.sadmag.macros_v2.date.DateUtils;
 import com.sadmag.macros_v2.equation.Equation;
 import com.sadmag.macros_v2.equation.exception.MissingValuesInEquation;
 import com.sadmag.macros_v2.user_info.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -10,11 +12,16 @@ import java.time.LocalDateTime;
 @Component
 public class HarrisBenedictEquationImpl implements Equation {
 
+    @Autowired
+    private DateUtils dateUtils;
+
     public float calculate(UserInfo userInfo) {
         var totalKcalSpent = 0.0f;
 
-        var now = LocalDateTime.now();
-        var yearToGetDate = userInfo.checkIfBirthHasOccuredThisYear() ? now.getYear() : now.getYear() - 1;
+        var now = dateUtils.getCurrentTime();
+
+        var yearToGetDate = dateUtils.hasDateOccurredThisYear(now, userInfo.getBirth()) ? now.getYear()
+                : now.getYear() - 1;
 
         var age = yearToGetDate - userInfo.getBirth().getYear();
         var gender = userInfo.getGender();
@@ -22,12 +29,10 @@ public class HarrisBenedictEquationImpl implements Equation {
         var weight = userInfo.getWeight();
 
         if (Character.toUpperCase(gender) == 'M') {
-            totalKcalSpent = 66 + (13.7f * weight) + (5 * height) - (6.8f * age);
-        }
-        else if (Character.toUpperCase(gender) == 'F') {
-            totalKcalSpent = 655 + (9.7f * weight) + (1.8f * height) - (4.7f * age);
-        }
-        else {
+            totalKcalSpent = 66.47f + (13.75f * weight) + (5.003f * height) - (6.775f * age);
+        } else if (Character.toUpperCase(gender) == 'F') {
+            totalKcalSpent = 655.09f + (9.563f * weight) + (1.85f * height) - (4.676f * age);
+        } else {
             throw new MissingValuesInEquation("Unrecognized or missing gender");
         }
 
