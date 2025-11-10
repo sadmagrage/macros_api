6406995a680759dfa2a1c3a15233b0fc83f5a8b9
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-10 01:35:13 -0300

    feat: add equation not found exception

---
+++ src/main/java/com/sadmag/macros_v2/equation/exception/EquationNotFound.java
@@ -0,0 +1,7 @@
+package com.sadmag.macros_v2.equation.exception;
+
+public class EquationNotFound extends RuntimeException {
+    public EquationNotFound(String message) {
+        super(message);
+    }
+}
