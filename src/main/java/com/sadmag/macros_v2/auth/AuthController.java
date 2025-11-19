31f9589dba96dadafa8f8769093d5716791d8380
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-19 02:22:19 -0300

    remove: remove the /register endpoint from /auth

--- src/main/java/com/sadmag/macros_v2/auth/AuthController.java
+++ src/main/java/com/sadmag/macros_v2/auth/AuthController.java
@@ -1,8 +1,5 @@
 package com.sadmag.macros_v2.auth;
 
-import com.sadmag.macros_v2.user.User;
-import com.sadmag.macros_v2.user.UserDto;
-import com.sadmag.macros_v2.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,20 +11,10 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @Autowired
-    private UserService userService;
-
     @PostMapping(path = "/login")
     public ResponseEntity<Object> login(@RequestBody AuthDto authDto) {
         var token = authService.authenticate(authDto);
 
         return ResponseEntity.ok(token);
-    }
-
-    @PostMapping(path = "/register")
-    public ResponseEntity<Object> register(@RequestBody UserDto newUser) {
-        userService.saveUser(newUser);
-
-        return ResponseEntity.created(null).build();
     }
 }
