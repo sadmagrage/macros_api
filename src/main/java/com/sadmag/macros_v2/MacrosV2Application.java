b3f3fc2944a9e3053cc5bb6eed7f6273d8f1d94b
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-19 01:42:10 -0300

    feat: allow spring to configure jpa

--- src/main/java/com/sadmag/macros_v2/MacrosV2Application.java
+++ src/main/java/com/sadmag/macros_v2/MacrosV2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class MacrosV2Application {
 
 	public static void main(String[] args) {
