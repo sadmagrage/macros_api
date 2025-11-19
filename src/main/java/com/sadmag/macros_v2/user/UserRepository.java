99d213b5647b958607a4125a16b29a32b6b0b64d
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-19 01:49:49 -0300

    feat: add method to receive user details

--- src/main/java/com/sadmag/macros_v2/user/UserRepository.java
+++ src/main/java/com/sadmag/macros_v2/user/UserRepository.java
@@ -1,13 +1,18 @@
 package com.sadmag.macros_v2.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Query("select u from User u where u.username = ?1")
+    UserDetails findUserDetailsByUsername(String username);
     Optional<User> findUserByUsername(String username);
     Optional<User> findUserByEmail(String email);
 }
