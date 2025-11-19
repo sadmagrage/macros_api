af3a0dd4ec84f4464e78da60cba00b5bb5ba850c
sadmagrage <victorgoyapvgp09@gmail.com>
2025-11-19 01:53:32 -0300

    feat: add token dto

---
+++ src/main/java/com/sadmag/macros_v2/token/TokenDto.java
@@ -0,0 +1,12 @@
+package com.sadmag.macros_v2.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    public String token;
+}
