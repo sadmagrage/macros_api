5fcdd396c82b48d487113c820b91f6d282f01f1f
sadmagrage <victorgoyapvgp09@gmail.com>
2025-12-08 00:20:49 -0300

    feat: add exception response

---
+++ src/main/java/com/sadmag/macros_v2/exceptions/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.sadmag.macros_v2.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private LocalDateTime timestamp;
+    private short status;
+    private String error;
+    private String message;
+    private String path;
+}
